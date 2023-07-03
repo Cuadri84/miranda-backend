ec3c8bc036e50c3f716a76126ebe9af7098a56b3
Cuadri84 <davidcuadrillerosaura@gmail.com>
2023-07-03 13:58:37 +0200

    MERGED SQL

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,6 +1,13 @@
+import crypto from "crypto";
 import fs from "fs";
+
 export const saveToDataBase = (value: any, filename: string) => {
   fs.writeFileSync(`./data/${filename}`, JSON.stringify(value), {
     encoding: "utf-8",
   });
 };
+
+export function hashPassword(password: string) {
+  const salt = crypto.randomBytes(16).toString("hex");
+  return crypto.pbkdf2Sync(password, salt, 1000, 64, "sha512").toString("hex");
+}
